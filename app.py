6181f347060862f5009f67b70085b7e7c26b0b69
Amanyadav-ds <aman.yadav.analytics@gmail.com>
2026-07-20 11:17:18 +0530

    Create initial Streamlit application

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+     page_title="Disaster Management System",
+     page_icon="🌏",
+     layout="wide"
+ )
+st.title("🌏 Disaster Management System")
+st.write("Welcome to the Disaster Relief Resource Management System.")
